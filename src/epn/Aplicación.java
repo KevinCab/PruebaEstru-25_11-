55dfce9eea47b0b1e7d8856be3ecbe940a1522ff
KevinCab <ultracacomon@hotmail.com>
2016-11-25 12:57:24 -0500

    Update Aplicación.java

--- src/epn/Aplicación.java
+++ src/epn/Aplicación.java
@@ -9,19 +9,23 @@ public class Aplicación {
 		
 		String Materias[]={"Calculo","Mates","Biologia"};
 		String Profesores[]={"Juan","Maria","Segoviano"};
+		String Alumnos[]={"Carlos","Juanca","Martha"};
 		Materia listaMaterias[]=new Materia[3];
 		Profesor listaProfesotres[]= new Profesor[5];
 		Alumno listaAlumnos[]= new Alumno[5];
 		
-		listaAlumnos[0]= new Alumno("Carlos",Materias,Profesores);
 		
+		
+		listaAlumnos[0]= new Alumno("Carlos",Materias,Profesores);
+		listaProfesotres[0]= new Profesor("Juan",Alumnos ,Materias);
 		
 		//Materias del Alumno 1(Posicion 0)
-		System.out.println(Arrays.toString(listaAlumnos[0].listMaterias()));
+		System.out.println("Materias del Alumno 1:\n"+ Arrays.toString(listaAlumnos[0].listMaterias()));
 		
+		//Materias del profesor 1(Posición 0)
+		System.out.println("Alumnos del Profesor 1\n"+Arrays.toString(listaProfesotres[0].listAlumnos()));
 		
-		
-		
+
 
 	}
 
