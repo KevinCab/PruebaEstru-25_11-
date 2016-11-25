be6a3ba0151a353bed23c558ce1325e85c16c9b1
KevinCab <ultracacomon@hotmail.com>
2016-11-25 12:50:11 -0500

    Update Aplicación.java

--- src/epn/Aplicación.java
+++ src/epn/Aplicación.java
@@ -1,9 +1,26 @@
 package epn;
 
-public class Aplicación {
+import java.util.Arrays;
+
+public class AplicaciÃ³n {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String Materias[]={"Calculo","Mates","Biologia"};
+		String Profesores[]={"Juan","Maria","Segoviano"};
+		Materia listaMaterias[]=new Materia[3];
+		Profesor listaProfesotres[]= new Profesor[5];
+		Alumno listaAlumnos[]= new Alumno[5];
+		
+		listaAlumnos[0]= new Alumno("Carlos",Materias,Profesores);
+		
+		
+		//Pprofesotres del Alumno 1(Posicion 0)
+		System.out.println(Arrays.toString(listaAlumnos[0].listProfesores()));
+		
+		
+		
 
 	}
 
