1d4d1ab30c48d65dbcc964c44eb986369e4a5667
KevinCab <ultracacomon@hotmail.com>
2016-11-25 12:33:22 -0500

    Update Alumno.java

--- src/epn/Alumno.java
+++ src/epn/Alumno.java
@@ -2,20 +2,28 @@ package epn;
 
 public class Alumno extends Persona {
 	String materias[];
+	String profesores[];
 	
-	public Alumno(String nombre, String lmaterias[]) {
+	public Alumno(String nombre, String[] materias, String[] profesores) {
 		super(nombre);
-		materias=lmaterias;
-		// TODO Auto-generated constructor stub
+		this.materias = materias;
+		this.profesores = profesores;
 	}
 
+	public String buscProfesor(Profesor profesor){
+		String busqueda = "";
+		
 	
+		return busqueda;
+	}
 	
+	public String[] listProfesores(){
+		return profesores;
+	}
 	
-	
-	
-	
-	
+	public String[] listMaterias(){
+		return materias;
+	}
 	
 
 }
