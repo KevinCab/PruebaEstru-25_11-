1dca813710ff4885321e7d45ed393bc9412b4cbf
SebastianMunozED <zeiko_munoz@hotmail.com>
2016-11-25 12:57:21 -0500

    Update Materia.java

--- src/epn/Materia.java
+++ src/epn/Materia.java
@@ -1,9 +1,30 @@
-package epn;
+package epn.com;
+
+import java.util.Arrays;
 
 public class Materia {
-	String nombMateria;
-	String profesores[];
-	String estudiantes[];
-	
+	String nombreMateria;
+	String [] profesor;
+	String [] alumno;
+
+	public Materia(String nombreMateria) {
+		super();
+		this.nombreMateria = nombreMateria;
+	}
+
+	public String getNombreMateria() {
+		return nombreMateria;
+	}
+
+	public void setNombreMateria(String nombreMateria) {
+		this.nombreMateria = nombreMateria;
+	}
+
+	@Override
+	public String toString() {
+		return "Materia [nombreMateria=" + nombreMateria + ", profesor=" + Arrays.toString(profesor) + ", alumno="
+				+ Arrays.toString(alumno) + "]";
+	}
+		
 
 }
